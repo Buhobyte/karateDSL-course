ac434bfddbf91a115c3ca1caabc706f930aafa72
buhobyte <byron.lopez.begins@gmail.com>
2022-05-11 00:32:24 -0500

    Thread Pool

--- src/test/java/ConduitTest/performance/PerformTest.scala
+++ src/test/java/ConduitTest/performance/PerformTest.scala
@@ -32,13 +32,13 @@ class PerformTest extends Simulation {
         // atOnceUsers(10),
         // rampUsers(10).during(5),
         constantUsersPerSec(1).during(2),
-        // constantUsersPerSec(2).during(10),
-        // // constantUsersPerSec(2).during(10).randomized(),
-        // rampUsersPerSec(2).to(10).during(20),
-        // // rampUsersPerSec(10).to(20).during(10).randomized(),
-        // nothingFor(5),
-        // // stressPeakUsers(1000).during(20),
-        // constantUsersPerSec(1).during(5),
+        constantUsersPerSec(2).during(10),
+        // constantUsersPerSec(2).during(10).randomized(),
+        rampUsersPerSec(2).to(10).during(20),
+        // rampUsersPerSec(10).to(20).during(10).randomized(),
+        nothingFor(5),
+        // stressPeakUsers(1000).during(20),
+        constantUsersPerSec(1).during(5),
       ).protocols(protocol)
   )
 
